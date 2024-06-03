c24da998d0f909d2ec843ead75eacc5a94da8cf3
Samar-Memon <samar56786raza@gmail.com>
2024-06-03 17:54:27 +0500

    Math_Game

---
+++ app.js
@@ -0,0 +1,270 @@
+let sound = new Audio('./click2.mp3');
+let trueSound = new Audio('./true.mp3');
+let wrongSound = new Audio('./wrong.mp3');
+
+let startBtn = document.getElementById('start');
+let gameSelect = document.getElementById('game');
+let levelSelect = document.getElementById('level');
+
+let startingArea = document.querySelector('.starting-area');
+
+let gameBar = document.querySelector('.gameBar');
+let headingBar = document.querySelector('.headingBar');
+let value1 = document.querySelector('.value1');
+let operator = document.querySelector('.operator');
+let value2 = document.querySelector('.value2');
+let option1 = document.querySelector('.option1');
+let option2 = document.querySelector('.option2');
+let option3 = document.querySelector('.option3');
+let options = document.querySelectorAll('.option');
+let sweetMain = document.querySelector('.sweetMain');
+
+let main = document.querySelector('.main');
+let waitingBar = document.querySelector('.waitingBar');
+
+setTimeout(() => {
+    waitingBar.style.display = 'none';
+    main.style.display = 'flex';
+}, 1000)
+
+let count = 0, right = 0, wrong = 0, currentLevel = 'easy';
+
+const gameBarGameDisplay = () => {
+    startingArea.style.transform = `scale(0)`;
+    setTimeout(() => startingArea.style.display = 'none', 300);
+
+    gameBar.style.display = 'flex';
+    setTimeout(() => gameBar.style.transform = `scale(1)`, 300)
+}
+let select = document.querySelectorAll('select').forEach((w) => {
+    w.addEventListener('click', () => {
+    sound.play();
+    })
+})
+
+options.forEach((a) => {
+    a.addEventListener('click', () => {
+        if (count < 10) {
+            let currentQuestion;
+
+            if (currentLevel === 'easy') {
+                if(gameSelect.value == 'divide'){
+                currentQuestion = allLevelDivideOperators[0][count];
+                }else if(gameSelect.value == 'multiply'){
+                currentQuestion = allLevelMultiplyOperators[0][count];
+                }else if(gameSelect.value == 'addition'){
+                currentQuestion = allLevelAdditionOperators[0][count];
+                }else if(gameSelect.value == 'subtraction'){
+                currentQuestion = allLevelSubtractionOperators[0][count];
+                }
+            } else if (currentLevel === 'normal') {
+                if(gameSelect.value == 'divide'){
+                currentQuestion = allLevelDivideOperators[1][count];
+                }else if(gameSelect.value == 'multiply'){
+                currentQuestion = allLevelMultiplyOperators[1][count];
+                }else if(gameSelect.value == 'addition'){
+                currentQuestion = allLevelAdditionOperators[1][count];
+                }else if(gameSelect.value == 'subtraction'){
+                currentQuestion = allLevelSubtractionOperators[1][count];
+                }
+            } else if (currentLevel === 'hard') {
+                if(gameSelect.value == 'divide'){
+                currentQuestion = allLevelDivideOperators[2][count];
+                }else if(gameSelect.value == 'multiply'){
+                currentQuestion = allLevelMultiplyOperators[2][count];
+                }else if(gameSelect.value == 'addition'){
+                currentQuestion = allLevelAdditionOperators[2][count];
+                }else if(gameSelect.value == 'subtraction'){
+                currentQuestion = allLevelSubtractionOperators[2][count];
+                }
+            } else if (currentLevel === 'extreme') {
+                if(gameSelect.value == 'divide'){
+                currentQuestion = allLevelDivideOperators[3][count];
+                }else if(gameSelect.value == 'multiply'){
+                currentQuestion = allLevelMultiplyOperators[3][count];
+                }else if(gameSelect.value == 'addition'){
+                currentQuestion = allLevelAdditionOperators[3][count];
+                }else if(gameSelect.value == 'subtraction'){
+                currentQuestion = allLevelSubtractionOperators[3][count];
+                }
+            }
+
+            if (a.textContent == currentQuestion.answer) {
+                a.style.background = 'green';
+                trueSound.play();
+                setTimeout(() => {
+                a.style.background = '';
+                }, 300)
+                right++;
+            } else {
+                a.style.background = 'red';
+                wrongSound.play();
+                setTimeout(() => {
+                a.style.background = '';
+                }, 300)
+                wrong++;
+            }
+
+            document.querySelector('.rightEle').innerHTML = right;
+            document.querySelector('.wrongEle').innerHTML = right;
+
+            count++;
+
+            document.getElementById('score').innerHTML = count;
+
+            // Call the appropriate function based on the current level
+            if (currentLevel === 'easy') {
+                if(gameSelect.value == 'divide'){
+                    divideEasyValues();
+                }else if(gameSelect.value == 'multiply'){
+                    multiplyEasyValues();
+                }else if(gameSelect.value == 'addition'){
+                    additionEasyValues();
+                }else if(gameSelect.value == 'subtraction'){
+                    subtractionEasyValues();
+                }
+            } else if (currentLevel === 'normal') {
+                if(gameSelect.value == 'divide'){
+                    divideNormalValues();
+                }else if(gameSelect.value == 'multiply'){
+                    multiplyNormalValues();
+                }else if(gameSelect.value == 'addition'){
+                    additionNormalValues();
+                }else if(gameSelect.value == 'subtraction'){
+                    subtractionNormalValues();
+                }
+            } else if (currentLevel === 'hard') {
+                if(gameSelect.value == 'divide'){
+                    divideHardValues();
+                }else if(gameSelect.value == 'multiply'){
+                    multiplyHardValues();
+                }else if(gameSelect.value == 'addition'){
+                    additionHardValues();
+                }else if(gameSelect.value == 'subtraction'){
+                    subtractionHardValues();
+                }
+            } else if (currentLevel === 'extreme') {
+                if(gameSelect.value == 'divide'){
+                    divideExtremeValues();
+                }else if(gameSelect.value == 'multiply'){
+                    multiplyExtremeValues();
+                }else if(gameSelect.value == 'addition'){
+                    additionExtremeValues();
+                }else if(gameSelect.value == 'subtraction'){
+                    subtractionExtremeValues();
+                }
+            }
+        }
+    });
+});
+
+
+
+
+const allOverFunction = () => {
+    if(gameSelect.value == 'divide' && levelSelect.value == 'easy'){
+        gameBarGameDisplay();
+        divideEasyValues();
+        currentLevel = 'easy';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Easy Divide Game`;
+    }else if(gameSelect.value == 'divide' && levelSelect.value == 'normal'){
+        gameBarGameDisplay();
+        divideNormalValues();
+        currentLevel = 'normal';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Normal Divide Game`;
+    }else if(gameSelect.value == 'divide' && levelSelect.value == 'hard'){
+        gameBarGameDisplay();
+        divideHardValues();
+        currentLevel = 'hard';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Hard Divide Game`;
+    }else if(gameSelect.value == 'divide' && levelSelect.value == 'extreme'){
+        gameBarGameDisplay();
+        divideExtremeValues();
+        currentLevel = 'extreme';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Extreme Divide Game`;
+    }else if(gameSelect.value == 'multiply' && levelSelect.value == 'easy'){
+        gameBarGameDisplay();
+        multiplyEasyValues();
+        currentLevel = 'easy';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Easy Multiply Game`;
+    }else if(gameSelect.value == 'multiply' && levelSelect.value == 'normal'){
+        gameBarGameDisplay();
+        multiplyNormalValues();
+        currentLevel = 'normal';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Normal Multiply Game`;
+    }else if(gameSelect.value == 'multiply' && levelSelect.value == 'hard'){
+        gameBarGameDisplay();
+        multiplyHardValues();
+        currentLevel = 'hard';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Hard Multiply Game`;
+    }else if(gameSelect.value == 'multiply' && levelSelect.value == 'extreme'){
+        gameBarGameDisplay();
+        multiplyExtremeValues();
+        currentLevel = 'extreme';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Extreme Multiply Game`;
+    }else if(gameSelect.value == 'addition' && levelSelect.value == 'easy'){
+        gameBarGameDisplay();
+        additionEasyValues();
+        currentLevel = 'easy';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Easy Addition Game`;
+    }else if(gameSelect.value == 'addition' && levelSelect.value == 'normal'){
+        gameBarGameDisplay();
+        additionNormalValues();
+        currentLevel = 'normal';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Normal Addition Game`;
+    }else if(gameSelect.value == 'addition' && levelSelect.value == 'hard'){
+        gameBarGameDisplay();
+        additionHardValues();
+        currentLevel = 'hard';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Hard Addition Game`;
+    }else if(gameSelect.value == 'addition' && levelSelect.value == 'extreme'){
+        gameBarGameDisplay();
+        additionExtremeValues();
+        currentLevel = 'extreme';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Extreme Addition Game`;
+    }else if(gameSelect.value == 'subtraction' && levelSelect.value == 'easy'){
+        gameBarGameDisplay();
+        subtractionEasyValues();
+        currentLevel = 'easy';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Easy Subtraction Game`;
+    }else if(gameSelect.value == 'subtraction' && levelSelect.value == 'normal'){
+        gameBarGameDisplay();
+        subtractionNormalValues();
+        currentLevel = 'normal';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Normal Subtraction Game`;
+    }else if(gameSelect.value == 'subtraction' && levelSelect.value == 'hard'){
+        gameBarGameDisplay();
+        subtractionHardValues();
+        currentLevel = 'hard';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Hard Subtraction Game`;
+    }else if(gameSelect.value == 'subtraction' && levelSelect.value == 'extreme'){
+        gameBarGameDisplay();
+        subtractionExtremeValues();
+        currentLevel = 'extreme';
+        headingBar.innerHTML = `<div class="score"><span id="score">0</span>/10</div> Extreme Subtraction Game`;
+}
+}
+
+
+startBtn.addEventListener('click', () => {
+    waitingBar.style.display = 'flex';
+    sound.play();
+    setTimeout(() => {
+        waitingBar.style.display = 'none';
+        allOverFunction();
+    }, 1000)
+    count = 0;
+    right = 0;
+    wrong = 0;
+});
+
+
+const sweetAlert = () => {
+    sweetMain.style.display = 'flex';
+    setTimeout(() => sweetMain.style.transform = `scale(1)`, 300);
+};
+
+const closePopup = () => {
+    sweetMain.style.transform = `scale(0)`;
+    setTimeout(() => sweetMain.style.display = 'none', 300);
+}
---
+++ divide.js
@@ -0,0 +1,119 @@
+let allLevelDivideOperators = [
+    [
+        {'value1': '2','operator': '÷','value2': '10','answer': '0.2','option1': '2','option2': '0.2','option3': '1.0'},
+        {'value1': '8','operator': '÷','value2': '3','answer': '2.6','option1': '2.6','option2': '3','option3': '2.3'},
+        {'value1': '1','operator': '÷','value2': '1','answer': '1','option1': '0.5','option2':'0','option3': '1',},
+        {'value1': '4','operator': '÷','value2': '5','answer': '0.8','option1': '0.3','option2': '0.7','option3': '0.8',},
+        {'value1': '10','operator': '÷','value2': '6','answer': '1.6','option1': '1.1','option2': '1.6','option3': '1',},
+        {'value1': '6','operator': '÷','value2': '9','answer': '0.6','option1': '0.6','option2': '1.9','option3': '0.9',},
+        {'value1': '2','operator': '÷','value2': '3','answer': '0.6','option1': '1.9','option2': '0.6','option3': '0.9',},
+        {'value1': '5','operator': '÷','value2': '7','answer': '0.7','option1': '0.7','option2': '1.9','option3': '0.6',},
+        {'value1': '3','operator': '÷','value2': '3','answer': '1','option1': '3','option2': '1','option3': '0',},
+        {'value1': '9','operator': '÷','value2': '4','answer': '2.25','option1': '1.97','option2': '2.25','option3': '1.45',},
+    ],
+    [
+        {'value1': '12','operator': '÷','value2': '29','answer': '0.4','option1': '1.29','option2': '0.4','option3': '0.69',},
+        {'value1': '14','operator': '÷','value2': '9','answer': '1.5','option1': '1.5','option2': '1.90','option3': '1.87',},
+        {'value1': '16','operator': '÷','value2': '56','answer': '0.28','option1': '1.99','option2': '0.28','option3': '2.89',},
+        {'value1': '5','operator': '÷','value2': '50','answer': '0.1','option1': '0.1','option2': '0.13','option3': '0.9',},
+        {'value1': '9','operator': '÷','value2': '18','answer': '0.5','option1': '0.90','option2': '0.34','option3': '0.5',},
+        {'value1': '7','operator': '÷','value2': '66','answer': '0.10','option1': '0.10','option2': '0.30','option3': '0.5',},
+        {'value1': '6','operator': '÷','value2': '89','answer': '0.06','option1': '0.09','option2': '0.06','option3': '2.10',},
+        {'value1': '18','operator': '÷','value2': '96','answer': '0.18','option1': '0.18','option2': '0.09','option3': '7.10',},
+        {'value1': '8','operator': '÷','value2': '36','answer': '0.20','option1': '0.09','option2': '0.20','option3': '0.90',},
+        {'value1': '19','operator': '÷','value2': '74','answer': '0.25','option1': '3.09','option2': '0.25','option3': '6.90',},
+    ],
+    // Hard questions
+    [
+        {'value1': '144', 'operator': '÷', 'value2': '12', 'answer': '12', 'option1': '12', 'option2': '10', 'option3': '11'},
+        {'value1': '256', 'operator': '÷', 'value2': '16', 'answer': '16', 'option1': '18', 'option2': '15', 'option3': '16'},
+        {'value1': '225', 'operator': '÷', 'value2': '15', 'answer': '15', 'option1': '14', 'option2': '15', 'option3': '16'},
+        {'value1': '289', 'operator': '÷', 'value2': '17', 'answer': '17', 'option1': '17', 'option2': '16', 'option3': '18'},
+        {'value1': '324', 'operator': '÷', 'value2': '18', 'answer': '18', 'option1': '17', 'option2': '19', 'option3': '18'},
+        {'value1': '361', 'operator': '÷', 'value2': '19', 'answer': '19', 'option1': '18', 'option2': '20', 'option3': '19'},
+        {'value1': '400', 'operator': '÷', 'value2': '20', 'answer': '20', 'option1': '21', 'option2': '20', 'option3': '19'},
+        {'value1': '441', 'operator': '÷', 'value2': '21', 'answer': '21', 'option1': '21', 'option2': '20', 'option3': '22'},
+        {'value1': '484', 'operator': '÷', 'value2': '22', 'answer': '22', 'option1': '23', 'option2': '22', 'option3': '21'},
+        {'value1': '529', 'operator': '÷', 'value2': '23', 'answer': '23', 'option1': '23', 'option2': '22', 'option3': '24'},
+    ],
+    // Extreme questions
+    [
+        {'value1': '123456', 'operator': '÷', 'value2': '123', 'answer': '1004', 'option1': '1004', 'option2': '1003', 'option3': '1005'},
+        {'value1': '654321', 'operator': '÷', 'value2': '123', 'answer': '5317', 'option1': '5316', 'option2': '5317', 'option3': '5318'},
+        {'value1': '987654', 'operator': '÷', 'value2': '246', 'answer': '4015', 'option1': '4015', 'option2': '4014', 'option3': '4016'},
+        {'value1': '543210', 'operator': '÷', 'value2': '135', 'answer': '4024', 'option1': '4023', 'option2': '4024', 'option3': '4025'},
+        {'value1': '789012', 'operator': '÷', 'value2': '234', 'answer': '3372', 'option1': '3372', 'option2': '3371', 'option3': '3373'},
+        {'value1': '135791', 'operator': '÷', 'value2': '111', 'answer': '1222', 'option1': '1221', 'option2': '1223', 'option3': '1222'},
+        {'value1': '246802', 'operator': '÷', 'value2': '202', 'answer': '1221', 'option1': '1220', 'option2': '1221', 'option3': '1222'},
+        {'value1': '369258', 'operator': '÷', 'value2': '321', 'answer': '1150', 'option1': '1150', 'option2': '1149', 'option3': '1151'},
+        {'value1': '654987', 'operator': '÷', 'value2': '567', 'answer': '1155', 'option1': '1154', 'option2': '1155', 'option3': '1156'},
+        {'value1': '888888', 'operator': '÷', 'value2': '444', 'answer': '2002', 'option1': '2001', 'option2': '2003', 'option3': '2002'},
+    ]
+];
+
+
+const divideEasyValues = () => {
+    if(count < 10){
+        value1.textContent = allLevelDivideOperators[0][count].value1;
+        operator.textContent = allLevelDivideOperators[0][count].operator;
+        value2.textContent = allLevelDivideOperators[0][count].value2;
+        option1.textContent = allLevelDivideOperators[0][count].option1;
+        option2.textContent = allLevelDivideOperators[0][count].option2;
+        option3.textContent = allLevelDivideOperators[0][count].option3;
+    } else{
+        gameBar.style.transform = `scale(0)`;
+        sweetAlert();
+
+        startingArea.style.display = 'flex';
+        setTimeout(() => startingArea.style.transform = `scale(1)`, 300);
+    }
+};
+
+const divideNormalValues = () => {
+    if(count < 10){
+        value1.textContent = allLevelDivideOperators[1][count].value1;
+        operator.textContent = allLevelDivideOperators[1][count].operator;
+        value2.textContent = allLevelDivideOperators[1][count].value2;
+        option1.textContent = allLevelDivideOperators[1][count].option1;
+        option2.textContent = allLevelDivideOperators[1][count].option2;
+        option3.textContent = allLevelDivideOperators[1][count].option3;
+    } else{
+        gameBar.style.transform = `scale(0)`;
+        sweetAlert();
+
+        startingArea.style.display = 'flex';
+        setTimeout(() => startingArea.style.transform = `scale(1)`, 300);
+    }
+};
+const divideHardValues = () => {
+    if(count < 10){
+        value1.textContent = allLevelDivideOperators[2][count].value1;
+        operator.textContent = allLevelDivideOperators[2][count].operator;
+        value2.textContent = allLevelDivideOperators[2][count].value2;
+        option1.textContent = allLevelDivideOperators[2][count].option1;
+        option2.textContent = allLevelDivideOperators[2][count].option2;
+        option3.textContent = allLevelDivideOperators[2][count].option3;
+    } else{
+        gameBar.style.transform = `scale(0)`;
+        sweetAlert();
+
+        startingArea.style.display = 'flex';
+        setTimeout(() => startingArea.style.transform = `scale(1)`, 300);
+    }
+};
+const divideExtremeValues = () => {
+    if(count < 10){
+        value1.textContent = allLevelDivideOperators[3][count].value1;
+        operator.textContent = allLevelDivideOperators[3][count].operator;
+        value2.textContent = allLevelDivideOperators[3][count].value2;
+        option1.textContent = allLevelDivideOperators[3][count].option1;
+        option2.textContent = allLevelDivideOperators[3][count].option2;
+        option3.textContent = allLevelDivideOperators[3][count].option3;
+    } else{
+        gameBar.style.transform = `scale(0)`;
+        sweetAlert();
+
+        startingArea.style.display = 'flex';
+        setTimeout(() => startingArea.style.transform = `scale(1)`, 300);
+    }
+};
